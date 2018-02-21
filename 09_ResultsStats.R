bb88ee3ea355dca6d6c54011c5aabbe685156d30
Kyle Chezik <kchezik@gmail.com>
2018-02-21 13:23:29 -0800

    added stats describing lower quartile contributions by group in match-mismatch

--- 09_ResultsStats.R
+++ 09_ResultsStats.R
@@ -22,3 +22,8 @@ perc = lapply(pstr$sigma, function(x){
 })
 
 test = test %>% mutate(tenP = round(Reduce("+", perc)/length(perc)*100,0))
+
+
+#Must run much of 06_Mismatch_Plot.R to get this statistic.
+mismatch %>% group_by(region) %>% summarise(max = max(perc, na.rm = T),
+																						lower = sum(perc<quantile(mismatch$perc, 0.25, na.rm = T), na.rm = T)/length(perc))
